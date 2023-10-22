1166ad59e5cdecb2e1c3b26d9b015afb0649a94b
bkubick <bjk2158@columbia.edu>
2023-10-22 18:23:15 -0500

    Update login-form to use store

--- web-app/src/views/business/forms/login-form.tsx
+++ web-app/src/views/business/forms/login-form.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
 import { ErrorMessage, Formik, Form, Field } from 'formik';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 
 import { InputField } from 'src/components/form/fields';
-import { Required, ValidationPipeline } from '../../../components/form/validation';
+import { Required } from 'src/components/form/validation';
+import * as user from 'src/store/user';
 
 
 interface FormValues {
@@ -11,7 +14,9 @@ interface FormValues {
 }
 
 
-interface Props {}
+interface Props {
+    login: (form: FormValues) => void;
+}
 
 
 interface State {
@@ -29,11 +34,18 @@ class LoginForm extends React.Component<Props, State> {
                 password: '',
             }
         }
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
-    onSubmit(values: FormValues, actions: any) {
+    /**
+     * Handles the form submission.
+     * 
+     * @param values 
+     * @param actions 
+     */
+    onSubmit(values: FormValues, actions: any): void {
         setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
+            this.props.login(values);
             actions.setSubmitting(false);
         }, 1000);
     }
@@ -43,7 +55,7 @@ class LoginForm extends React.Component<Props, State> {
             <Formik initialValues={ this.state.form } onSubmit={ this.onSubmit }>
                 <Form className='w-full p-10'>
                     <div className='mb-4'>
-                        <Field name="username" placeholder="Username/Email" component={ InputField } validate={ ValidationPipeline([Required]) }/>
+                        <Field name="username" placeholder="Username/Email" component={ InputField } validate={ Required }/>
                         <ErrorMessage name="username"/>
                     </div>
                     <div className='mb-4'>
@@ -58,4 +70,19 @@ class LoginForm extends React.Component<Props, State> {
 }
 
 
-export default LoginForm;
+/**
+ * Dispatches the login action to the store.
+ * 
+ * TODO: Need to figure out how to handle the 'any' type here.
+ * 
+ * @param dispatch 
+ * @returns 
+ */
+const mapDispatchToProps = (dispatch: Dispatch<any>) => {
+    return {
+        login: (form: FormValues) => dispatch(user.actions.login({ ...form })),
+    }
+};
+
+
+export default connect(null, mapDispatchToProps)(LoginForm);
